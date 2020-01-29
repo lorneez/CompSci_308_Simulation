3044c6743eb46e606dc991ae0ebf5656248dfb95
Cary Shindell <css57@duke.edu>
2020-01-28 22:28:36 -0500

    added comment in update method

--- src/cellsociety/Model/Grid.java
+++ src/cellsociety/Model/Grid.java
@@ -44,6 +44,8 @@ public class Grid {
     public void updateGrid(){
         for(int i=0; i<colSize; i++){
             for(int j=0; j<rowSize; j++){
+                // in order to keep both updating and calculating next state in the same iteration, the cell is updated
+                // and then the next state is calculated. Note that therefore nothing will happen on the first step
                 cells[i][j].update();
                 cells[i][j].calculateNextState();
             }
