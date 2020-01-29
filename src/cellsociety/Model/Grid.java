32efb1800c031d4e9259bebbc86dcc218d55e9a1
Cary Shindell <css57@duke.edu>
2020-01-28 22:53:28 -0500

    made grid abstract and added firegrid class

--- src/cellsociety/Model/Grid.java
+++ src/cellsociety/Model/Grid.java
@@ -1,29 +1,44 @@
 package cellsociety.Model;
 
 
-public class Grid {
+import java.util.ArrayList;
+
+public abstract class Grid {
     public Cell[][] cells;
     private int rowSize;
     private int colSize;
 
 
-    public Grid(int rowSize, int colSize){
+    public Grid(int rowSize, int colSize, ArrayList<Integer> initial_positions){
         this.rowSize = rowSize;
         this.colSize = colSize;
         this.cells = new Cell[rowSize][colSize];
+        initializeGrid(initial_positions);
     }
 
-    public void initializeGrid(ArrayList initial_positions){
-        int index = 0;
+    public void updateGrid(){
         for(int i=0; i<colSize; i++){
             for(int j=0; j<rowSize; j++){
-                cells[i][j] = initial_positions.get(index);
-                index ++;
+                // in order to keep both updating and calculating next state in the same iteration, the cell is updated
+                // and then the next state is calculated. Note that therefore nothing will happen on the first step
+                cells[i][j].update();
+                cells[i][j].calculateNextState();
             }
         }
     }
 
-    public void setNeighbors(){
+    private void initializeGrid(ArrayList<Integer> initial_positions){
+        int index = 0;
+        for(int i=0; i<colSize; i++){
+            for(int j=0; j<rowSize; j++){
+                cells[i][j] = makeCell(initial_positions.get(index));
+                index ++;
+            }
+        }
+        setNeighbors();
+    }
+
+    private void setNeighbors(){
         for(int i=0; i<colSize; i++){
             for(int j=0; j<rowSize; j++){
                 if(j>1){
@@ -41,16 +56,8 @@ public class Grid {
             }
         }
     }
-    public void updateGrid(){
-        for(int i=0; i<colSize; i++){
-            for(int j=0; j<rowSize; j++){
-                // in order to keep both updating and calculating next state in the same iteration, the cell is updated
-                // and then the next state is calculated. Note that therefore nothing will happen on the first step
-                cells[i][j].update();
-                cells[i][j].calculateNextState();
-            }
-        }
-    }
+
+    public abstract Cell makeCell(int state);
 
     // public void setDiagnonalNeighbors
 
