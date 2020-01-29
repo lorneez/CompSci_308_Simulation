63a634a5bf64bee5f11d44cd62c1b6101ef40bb8
Cary Shindell <css57@duke.edu>
2020-01-29 18:40:38 -0500

    started adding to GridViewer

--- src/cellsociety/View/GridViewer.java
+++ src/cellsociety/View/GridViewer.java
@@ -1,10 +1,21 @@
 package cellsociety.View;
 
+import javafx.scene.Scene;
 import javafx.scene.paint.Paint;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
+import java.awt.*;
 import java.util.Map;
 
 public class GridViewer {
 
-    private final Map<Integer, Paint> colorMap;
+    private static final Paint[] COLORMAP = {Color.BLACK, Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.BLUE};
+
+    private Stage myStage;
+    private Scene myScene;
+
+    public GridViewer(){
+
+    }
 }
