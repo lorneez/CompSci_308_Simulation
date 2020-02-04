082743c1953bd8cc8d1c7ae042cd020a3b8552b3
Ameer Syedibrahim <as877@duke.edu>
2020-02-03 22:58:22 -0500

    core functionality for predator and prey complete along with xml config file

--- src/cellsociety/Model/Cell/PredatorPreyCell.java
+++ src/cellsociety/Model/Cell/PredatorPreyCell.java
@@ -14,7 +14,7 @@ public class PredatorPreyCell extends Cell{
     private static double energy;
     private static double chronons_passed;
     private final int REPRODUCTION_THRESHOLD = 6; // chronon threshold, not sure what this number should be
-    private final int FISH_ENERGY = 10;
+    private final int FISH_ENERGY = 7;
     private boolean canReproduce = false;
 
     /**
@@ -23,8 +23,14 @@ public class PredatorPreyCell extends Cell{
      */
     public PredatorPreyCell(int state){
         super(state);
-
         // if state is shark, set energy to 5
+
+        if (this.currentState == 0){
+            energy = 5;
+        }
+
+        this.chronons_passed = 0;
+
     }
 
     public int calculateNextState(){
@@ -40,7 +46,7 @@ public class PredatorPreyCell extends Cell{
 
                 if (canReproduce){
                     the_neighbor.chronons_passed = 0;
-                    the_neighbor.canReproduce = true;
+                    the_neighbor.canReproduce = false;
 
                     this.currentState = 2;
                     this.chronons_passed = 0;
@@ -52,6 +58,7 @@ public class PredatorPreyCell extends Cell{
                     this.chronons_passed = 0;
                 }
             }else{
+
                 this.chronons_passed+=1;
             }
 
@@ -68,20 +75,21 @@ public class PredatorPreyCell extends Cell{
             if (this.energy == 0){
                 this.currentState = 5;
                 this.chronons_passed = 0;
+                this.canReproduce = false;
             }
             // if there is an adjacent square occupied by a fish, the shark will move there randomly
             else if (neighborFish()!= null){
                 PredatorPreyCell the_fish_neighbor = neighborFish();
 
                 the_fish_neighbor.currentState = 0;
-                the_fish_neighbor.chronons_passed = this.chronons_passed+=1;
-                the_fish_neighbor.energy = this.energy-=1 + FISH_ENERGY;
+                the_fish_neighbor.chronons_passed = this.chronons_passed+ 1;
+                the_fish_neighbor.energy = this.energy - 1 + FISH_ENERGY;
                 this.chronons_passed = 0;
                 this.currentState = 5;
 
             }
             // if the shark can reproduce AND if no fish are neighbors, randomly move to an unoccupied square
-            else if (neighborFish() == null && neighborWater() != null && this.canReproduce){
+            else if (neighborFish() == null && neighborWater() != null){
                 PredatorPreyCell the_neighbor = neighborWater();
                 the_neighbor.currentState = 0;
 
@@ -93,15 +101,15 @@ public class PredatorPreyCell extends Cell{
                     this.currentState = 0;
                     this.canReproduce = false;
                 }else{
-                    the_neighbor.chronons_passed = this.chronons_passed+=1;
-                    the_neighbor.energy = this.energy-=1;
+                    the_neighbor.chronons_passed = this.chronons_passed+1;
+                    the_neighbor.energy = this.energy-1;
 
                     this.currentState = 5;
                 }
                 this.chronons_passed = 0;
 
             }
-            // if the fish can't move anywhere, update the number of chronons
+            // if the shark can't move anywhere, update the number of chronons
             else if(neighborFish() == null && neighborWater() == null){
                 this.chronons_passed+=1;
             }
@@ -114,22 +122,19 @@ public class PredatorPreyCell extends Cell{
         return currentState;
     }
 
-    // return an integer which indicates that if any of the 4 neighbors is empty (the state is 0)
-    // return if there are many, randomly select a neighbor
-    // return 1 for left, 2 for right, 3 for up, 4 for down
     public PredatorPreyCell neighborWater(){
         ArrayList<PredatorPreyCell> openWater = new ArrayList<>();
 
-        if(cellIsWater(this.leftNeighbor)){
+        if( this.leftNeighbor != null && cellIsWater(this.leftNeighbor)){
             openWater.add(this.leftNeighbor);
         }
-        if(cellIsWater(this.rightNeighbor)){
+        if( this.rightNeighbor != null && cellIsWater(this.rightNeighbor)){
             openWater.add(this.rightNeighbor);
         }
-        if(cellIsWater(this.upperNeighbor)){
+        if( this.upperNeighbor != null && cellIsWater(this.upperNeighbor)){
             openWater.add(this.upperNeighbor);
         }
-        if(cellIsWater(this.lowerNeighbor)){
+        if( this.lowerNeighbor != null && cellIsWater(this.lowerNeighbor)){
             openWater.add(this.lowerNeighbor);
         }
 
@@ -141,22 +146,19 @@ public class PredatorPreyCell extends Cell{
         return null;
     }
 
-    // return an integer which indicates that if any of the 4 neighbors is a fish (the state is 1)
-    // return if there are many, randomly select a neighbor
-    // return 1 for left, 2 for right, 3 for up, 4 for down
     public PredatorPreyCell neighborFish(){
         ArrayList<PredatorPreyCell> nearbyFishes = new ArrayList<>();
 
-        if(cellIsFish(this.leftNeighbor)){
+        if(this.leftNeighbor != null && cellIsFish(this.leftNeighbor)){
             nearbyFishes.add(this.leftNeighbor);
         }
-        if(cellIsFish(this.rightNeighbor)){
+        if(this.rightNeighbor != null && cellIsFish(this.rightNeighbor)){
             nearbyFishes.add(this.rightNeighbor);
         }
-        if(cellIsFish(this.upperNeighbor)){
+        if( this.upperNeighbor != null && cellIsFish(this.upperNeighbor)){
             nearbyFishes.add(this.upperNeighbor);
         }
-        if(cellIsFish(this.lowerNeighbor)){
+        if(this.lowerNeighbor != null && cellIsFish(this.lowerNeighbor)){
             nearbyFishes.add(this.lowerNeighbor);
         }
 
@@ -175,7 +177,5 @@ public class PredatorPreyCell extends Cell{
     public boolean cellIsFish(PredatorPreyCell cell){
         return cell.getCurrentState() == 2;
     }
-
-
 
 }
