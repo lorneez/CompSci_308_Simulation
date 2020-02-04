19b9b09b39331e55286b4ba1eb79fb2210bf698e
Lorne Zhang <lz150@duke.edu>
2020-02-04 01:13:21 -0500

    added javadoc to segregation and implemented is done function

--- src/cellsociety/Model/Cell/SegregationCell.java
+++ src/cellsociety/Model/Cell/SegregationCell.java
@@ -1,13 +1,19 @@
 package cellsociety.Model.Cell;
 import cellsociety.Model.Cell.Cell;
 
+/**
+ * Class representing a cell object for the segregation simulation
+ * @author caryshindell, lornezhang, ameersyedibrahim
+ * Dependencies: Cell class
+ * Example: a segregation cell with 3 neighbors in various states
+ */
 public class SegregationCell extends Cell{
     private static double satisfy;
-    public static final int red = 1;
-    public static final int blue = 5;
-    public static final int rednotsatisfied = 0;
-    public static final int bluenotsatisfied = 4;
-    public static final int empty = 7;
+    public static final int RED_STATE = 1;
+    public static final int BLUE_STATE = 5;
+    public static final int REDNOTSATISFIED_STATE = 0;
+    public static final int BLUENOTSATISFIED_STATE = 4;
+
     /**
      * Cell Constructor
      * @param state
@@ -16,12 +22,26 @@ public class SegregationCell extends Cell{
         super(state);
     }
 
+    /**
+     * sets the satisfy variable
+     * @param param1 the percentage of satisfication
+     */
     public static void setProb(double param1){
         satisfy = param1;
     }
-    public void setCurrentState(int x){
-        currentState = x;
+
+    /**
+     * sets the currrent state
+      * @param state the current state
+     */
+    public void setCurrentState(int state){
+        currentState = state;
     }
+
+    /**
+     * Calculates the next state of the cell and checks if the cell is satisfied.
+     * @return the current state of the cell after checking if it is satisfied
+     */
     @Override
     public int calculateNextState(){
         Cell[] neighbors = getAllNeighbors();
@@ -42,12 +62,15 @@ public class SegregationCell extends Cell{
         return currentState;
     }
 
+    /**
+     * Sets the cell's next state to the not satisfied variable corresponding to its color.
+     */
     private void setNotSatisfied() {
-        if(currentState == red){
-            currentState = rednotsatisfied;
+        if(currentState == RED_STATE){
+            currentState = REDNOTSATISFIED_STATE;
         }
-        else if(currentState == blue){
-            currentState = bluenotsatisfied;
+        else if(currentState == BLUE_STATE){
+            currentState = BLUENOTSATISFIED_STATE;
         }
     }
 }
